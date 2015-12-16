258c11b371a427cad6dc460c5c1e8a7770f4f51c
Kilian BEUCHARD <kbeuchar@lpnws5220.in2p3.fr>
2015-12-16 13:56:27 +0100

    Updating comments in boxcar_extraction.py

--- bin/boxcar_extraction.py
+++ bin/boxcar_extraction.py
@@ -5,7 +5,7 @@ import astropy.io.fits as pyfits
 import pylab
 from numpy.polynomial.legendre import legval, legfit
 
-def u(wave,wavemin,wavemax) :
+def u(wave, wavemin, wavemax) :
     return 2.*(wave-wavemin)/(wavemax-wavemin)-1.
 
 ################
@@ -30,47 +30,65 @@ def boxcar(file_1, file_2) :
     wavemax = psf[0].header["WAVEMAX"]
     xcoef   = psf[0].data
     ycoef   = psf[1].data
-    xsig    = psf[2].data       	
+    xsig    = psf[2].data
 #    print xcoef.shape
     image_file  = pyfits.open(file_2)
     flux        = image_file[0].data
-    flux_ivar   = image_file[1].data      # inverse de la variance de la valeur des pixels de l'image
+    #   Inverse-Variance of pixel's value from file_2
+    flux_ivar   = image_file[1].data
+    #   Variance to-be-filled sizeof Inverse-Variance
     flux_var    = np.zeros(flux_ivar.shape)
-    mask        = (flux_ivar > 0)
 
+    mask        = (flux_ivar > 0)
     flux_var[mask] = 1./flux_ivar[mask]
 
     npix_y  = flux.shape[0]
     npix_x  = flux.shape[1]
-    wave=np.linspace(wavemin,wavemax,10)
+    wave    = np.linspace(wavemin, wavemax, 10)
 
-    spectra             = np.zeros((nfibers,npix_y)) # Flux as a function of wavelength
-    spectra_ivar        = np.zeros((nfibers,npix_y)) # Reverse variance of spectrum
-    wave_vs_y_of_fibers = np.zeros((nfibers,npix_y))
+    #   Flux as a function of wavelength
+    spectra             = np.zeros((nfibers,npix_y))
+    #   Inverse-Variance of spectrum
+    spectra_ivar        = np.zeros((nfibers,npix_y))
+    #   Wavelength
+    wave_of_y           = np.zeros((nfibers, npix_y))
+    #   Resolution matrix
+    #rdata               = np.zeros((nfibers, 0, npix_y))    #   '0' is to be changed
 
-# Using legendre's polynomial to get a spectrum per fiber.
+###
+# Using legendre's polynomial to get a spectrum per fiber
+###
 
     for fiber in xrange(nfibers) :
         print fiber
-        y_of_wave   = legval(u(wave, wavemin, wavemax), ycoef[fiber])
-        coef        = legfit(u(y_of_wave, 0, npix_y), wave, deg=ycoef[fiber].size)
-        wave_of_y   = legval(u(np.arange(npix_y).astype(float), 0, npix_y), coef)
-        x_of_y      = legval(u(wave_of_y, wavemin, wavemax), xcoef[fiber])
-        x1_of_y     = np.floor(x_of_y).astype(int) - 3
-        x2_of_y     = np.floor(x_of_y).astype(int) + 4
+        y_of_wave           = legval(u(wave, wavemin, wavemax), ycoef[fiber])
+        coef                = legfit(u(y_of_wave, 0, npix_y), wave, deg=ycoef[fiber].size)
+        #   Determines wavelength position on Y
+        wave_of_y[fiber]    = legval(u(np.arange(npix_y).astype(float), 0, npix_y), coef)
+        #   Determines wavelength intensity (x) based on Y
+        x_of_y              = legval(u(wave_of_y[fiber], wavemin, wavemax), xcoef[fiber])
+        #   Ascertain X by using low and high uncertainty
+        x1_of_y             = np.floor(x_of_y).astype(int) - 3
+        x2_of_y             = np.floor(x_of_y).astype(int) + 4
         for y in xrange(npix_y) :
-            nb_invalidPix=np.sum(flux_ivar[y,x1_of_y[y]:x2_of_y[y]] <= 0) # number of pixels with neg or null flux_ivar
+            #   Number of dead pixel in flux_ivar
+            nb_invalidPix   = np.sum(flux_ivar[y, x1_of_y[y]:x2_of_y[y]] <= 0)
+            #   If there's no dead pixel:
             if nb_invalidPix == 0 :
-                spectra[fiber, y] = np.sum(flux[y,x1_of_y[y]:x2_of_y[y]])       # sum of flux
-                var               = np.sum(flux_var[y,x1_of_y[y]:x2_of_y[y]])   # sum of variances
+                #   Sum of flux
+                spectra[fiber, y]   = np.sum(flux[y, x1_of_y[y]:x2_of_y[y]])
+                #   Sum of Variance
+                var                 = np.sum(flux_var[y, x1_of_y[y]:x2_of_y[y]])
                 spectra_ivar[fiber, y] = 1./var
-        wave_vs_y_of_fibers[fiber]= wave_of_y
+#                rdata               = (fiber, ndiag, y)
 
     hdulist=pyfits.HDUList([pyfits.PrimaryHDU(spectra),
                             pyfits.ImageHDU(spectra_ivar,name="IVAR"),
-                            pyfits.ImageHDU(wave_vs_y_of_fibers,name="WAVELENGTH")])
-    # HDU[0]: Spectra
-    # HDU[1]: Inverse variance of spectra
-    # HDU[2]: Wavelengh
-    # HDU[3]: Resolution matrix
+                            pyfits.ImageHDU(wave_of_y,name="WAVELENGTH")])
+                            #pyfits.ImageHDU(rdata, name="RESOLUTION")])
+    # hdulist[0]: Spectra
+    # hdulist[1]: Inverse variance of spectra
+    # hdulist[2]: Wavelengh
+    # hdulist[3]: Resolution matrix
+    #       =   Rdata[nspec, ndiag, nwave]
     hdulist.writeto("frame.fits",clobber=True)

c22008ffb05443ba1b5980bfabbe1314f77bfb85
Julien Guy <jguy@lbl.gov>
2016-01-14 12:49:41 +0100

    tmp improvements

--- py/desispec_tools/graph_tools.py
+++ py/desispec_tools/graph_tools.py
@@ -1,5 +1,7 @@
 import pylab
 from desispec.log import get_logger
+import sys
+import numpy as np
 
 def plot_graph(frame, nfibers=None, start=0, end=None) :
     """Plot graph from a given spectra from a fits file.
@@ -29,25 +31,66 @@ def plot_graph(frame, nfibers=None, start=0, end=None) :
         if nfibers > f_shape :
             log.warning("only %d fibers will be shown" % f_shape)
         f_shape = min(nfibers, f_shape)
+    
+    #   These values come from a string.
+    #   They need this to be treated as integer.
+    start   = int(start)
+    end     = int(end)
 
     #   If end is not defined, end may take nfibers value
     #   else it will take max number of fibers value
     if end is None :
         end = f_shape
-
+    elif end > f_shape :
+        log.warning("will plot only %d fibers"%f_shape)
+        end = f_shape
+    
     if start > end :
         log.error("Warning. Nothing will be plotted since begin value is higher than end value")
    
-    #   These values come from a string.
-    #   They need this to be treated as integer.
-    start   = int(start)
-    end     = int(end)
+   
     
-    print ivar
+    show_errors = True
+    
+    if show_errors :
+        err = np.sqrt(1./(ivar+(ivar==0)))*(ivar>0)
+        
+    
     #print len(spectra[0])
-    for fiber in xrange(f_shape) :
-        if (fiber >= start and fiber <= end) :
-            log.info("Plotting fiber %03d" % fiber)
-            pylab.plot(spectra[fiber], 'b')
-            pylab.plot(ivar[fiber], 'r')
+    for fiber in xrange(start,end) :
+        log.info("Plotting fiber %03d" % fiber)
+            
+        if show_errors :
+                
+            if len(wave.shape)>1 :
+                pylab.errorbar(wave[fiber],spectra[fiber],err[fiber],fmt="o-")
+            else :
+                pylab.errorbar(wave,spectra[fiber],err[fiber],fmt="o-")
+        else :
+            if len(wave.shape)>1 :
+                pylab.plot(wave[fiber],spectra[fiber],"-")
+            else :
+                pylab.plot(wave,spectra[fiber],"-")
+    
+    pylab.xlabel("Wavelength [A]")
+
+
+    show_2d=True
+
+    if show_2d :
+        pylab.figure()
+        if len(wave.shape)==1 :
+            print wave[0],wave[-1],start-0.5,end-0.5
+            print spectra[start:end].shape
+            pylab.imshow(spectra[start:end],aspect=spectra.shape[1]/float(end-start),extent=(wave[0],wave[-1],start-0.5,end-0.5),origin=0.,interpolation="nearest")
+            pylab.xlabel("Wavelength [A]")
+            pylab.ylabel("Fiber #")
+        else :
+            pylab.imshow(spectra[start:end],aspect=spectra.shape[1]/float(end-start),extent=(0,spectra.shape[1],start-0.5,end-0.5),origin=0.,interpolation="nearest")
+            pylab.xlabel("Y CCD")
+            pylab.ylabel("Fiber #")
+        pylab.colorbar()
+            
+
+
     pylab.show()
